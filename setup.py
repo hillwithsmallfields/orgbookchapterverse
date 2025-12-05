48c92bb0d8d953cdd35e94f07d34a07058c0fc0a
John Sturdy <jcg.sturdy@gmail.com>
2025-12-05 18:08:46 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="orgbookchapterverse",
+    version="0.1",
+    description="Handle books in book-chapter-verse form in org-mode files.",
+    author="John C. G. Sturdy",
+    author_email="jcg.sturdy@gmail.com",
+    packages=find_packages(),
+)

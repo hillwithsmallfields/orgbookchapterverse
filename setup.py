0cf233107cb1b700d6943a5a50b8edaf1ca4dec7
John Sturdy <jcg.sturdy@gmail.com>
2026-06-15 10:19:19 +0100

    placate build

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="orgbookchapterverse",
+    version="0.1.0",
     description="Handle books in book-chapter-verse form in org-mode files.",
     author="John C. G. Sturdy",
     author_email="jcg.sturdy@gmail.com",
